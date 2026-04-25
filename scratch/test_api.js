6481e47afbc6a98198150b7fab797f83f63cc860
patto220048 <220048xn@yse-c.net>
2026-04-25 09:25:30 +0700

    feat: add supabase skills and update gitignore for environment files

--- scratch/test_api.js
+++
@@ -1,8 +0,0 @@
-import { getResources } from './app/lib/api.js';
-
-async function test() {
-  const res = await getResources({ limit: 5 });
-  console.log(JSON.stringify(res, null, 2));
-}
-
-test();
